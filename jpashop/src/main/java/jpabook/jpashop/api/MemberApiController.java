a53c3ee7bdcca9f2d3ada800c118c106e650cf5b
nokh9503 <nokh9503@hanmail.net>
2022-05-10 23:59:40 +0900

    [add] API 개발 - 회원 조회 API

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,12 +9,51 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    /**
+     * 회원 조회 API v1 (안좋은 예시)
+     * @return
+     */
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    /**
+     * 회원 조회 API v2 (개선된 예시)
+     * @return
+     */
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        // 노출할 것만 스펙에 노출. 필요한 것만 노출
+        private String name;
+    }
 
     @PostMapping("/api/v1/members")
     // requestBody: Json으로 온 body를 member로 변환해줌
--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,9 +20,11 @@ public class Member {
 //    @NotEmpty
     private String name;
 
+    @JsonIgnore
     @Embedded   // 내장 타입임을 표시
     private Address address;
 
+//    @JsonIgnore // 엔티티의 정보를 뺌
     @OneToMany(mappedBy = "member")  // 일 대 다, 연관관계 주인 표시
     private List<Order> orders = new ArrayList<>();
 }
