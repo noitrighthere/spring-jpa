b2c8cff3adca4ae6703680042d6960166de63d50
nokh9503 <nokh9503@hanmail.net>
2022-05-22 22:46:30 +0900

    add 영속성 관리 - 내부 동작 방식

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -17,14 +17,13 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member member = new Member();
-//            member.setId(1L);
-//            member.setName("HelloA");
 
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJPA");
+            // 영속
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
 
-//            em.persist(member);
+            em.persist(member1);
+            em.persist(member2);
 
             tx.commit();
         } catch (Exception e) {
